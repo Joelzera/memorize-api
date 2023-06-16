bb1f06642dab77481a491d3e86f77ffe91e648e3
joel bispo <joelbispo04@hotmail.com>
2023-06-15 23:01:04 -0300

    rota de criação de usuarios

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const mysql = require('./config/db.config')
 
 const app = express()
 const port = process.env.SERVER_PORT || 5000
@@ -8,8 +9,30 @@ const port = process.env.SERVER_PORT || 5000
 app.use(bodyParser.urlencoded({ extended: true })) // permite aceitar requisições do tipo x-www-form-urlencoded
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send('hello, world')
+
+
+app.post('/user', async (req, res) => {
+    try {
+        const query = `
+            INSERT INTO users
+                        (active, email, phone, name, last_name, picture, created_at)
+                 VALUES (?, ?, ?, ?, ?, ?, ?);`
+        const resultado = await mysql.execute(
+            query,
+            [
+                true,
+                req.body.email,  
+                req.body.phone,  
+                req.body.name,  
+                req.body.last_name,  
+                req.body.picture,  
+                new Date().toISOString().slice(0, 19).replace('T', ' ')
+            ]
+        )
+        res.send(resultado)
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 app.listen(port, () => {
