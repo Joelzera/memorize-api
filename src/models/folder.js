e994c80c461433c150849bbb652b2dc3de314834
joel bispo <joelbispo04@hotmail.com>
2023-06-27 22:39:29 -0300

    criação de modelos folder/annotation

---
+++ src/models/folder.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+
+const dataSchema = new mongoose.Schema({
+    id: { type: String },
+    idUser: { type: String },
+    name: { type: String },
+    annotationIds: { type: [String] },
+    createdAt: { type: Date },
+    updatedAt: { type: Date }
+})
+
+
+module.exports = mongoose.model('Folder', dataSchema)
