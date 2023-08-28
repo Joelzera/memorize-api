599814f2e9870aa5a1f45509d3d39ade0a139115
joel bispo <joelbispo04@hotmail.com>
2023-08-28 10:35:50 -0300

    configuração do cors e nova rota

--- src/server.js
+++ src/server.js
@@ -4,12 +4,19 @@ const bodyParser = require('body-parser')
 const userRoutes = require('./routes/user')
 const folderRoutes = require('./routes/folder')
 const annotationRoutes = require('./routes/annotation')
-
+const cors = require('cors')
 const app = express()
 const port = process.env.SERVER_PORT || 5000
 
+
 app.use(bodyParser.urlencoded({ extended: true })) // permite aceitar requisições do tipo x-www-form-urlencoded
 app.use(bodyParser.json())
+app.use(cors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true
+}))
 app.use('/', userRoutes)
 app.use('/', folderRoutes)
 app.use('/', annotationRoutes)
